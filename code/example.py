3fadfbc3bbad54823f0c4f0883c72e673b6c520a
ucyo <cayoglu@me.com>
2020-08-27 16:05:27 +0200

    Black fmt

--- code/example.py
+++ code/example.py
@@ -4,12 +4,12 @@
 from datarequests.era5 import ERA5PressureLevelsRequest
 
 req = ERA5PressureLevelsRequest(
-    variable=['temperature'],
-    year = [1986],
-    month = [1],
-    day = [17],
-    time = ['11:00'],
-    pressure_level=[800]
+    variable=["temperature"],
+    year=[1986],
+    month=[1],
+    day=[17],
+    time=["11:00"],
+    pressure_level=[800],
 )
 
 req.send_request("/downloads/redic.nc")
